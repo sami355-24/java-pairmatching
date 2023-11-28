bf3be4ee6fd5f25a29424cb167234109d7dfc1ed
sami355-24 <ddung355@gmail.com>
2023-11-28 13:59:12 +0900

    feat: 에러 발생시 발생시킬 사용자 정의 예외 클래스 생성

---
+++ src/main/java/pairmatching/Exception/CommonValidateException.java
@@ -0,0 +1,8 @@
+package org.example.Exception;
+
+public class CommonValidateException extends IllegalArgumentException{
+
+    public CommonValidateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
