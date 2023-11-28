4db0de52c35f61f9a2cc8397ac772b7ac2e625b8
sami355-24 <ddung355@gmail.com>
2023-11-28 14:00:29 +0900

    feat: 여러 패키지에서 사용할 공통 기능 관리하는 패키지 추가

---
+++ src/main/java/pairmatching/Common/Utility.java
@@ -0,0 +1,11 @@
+package pairmatching.Common;
+
+public class Utility {
+
+    private Utility(){
+    }
+
+    public static int convertStringToInt(String str) {
+        return Integer.parseInt(str);
+    }
+}
