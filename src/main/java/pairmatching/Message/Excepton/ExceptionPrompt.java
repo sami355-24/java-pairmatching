cfe7e8abc8ef7055ad877b6c83edd8e39158e0fd
sami355-24 <ddung355@gmail.com>
2023-12-05 17:19:42 +0900

    feat: 기능을 선택할때 필요한 문구들 관리하는 패키지 생성

--- src/main/java/pairmatching/Message/Excepton/ExceptionPrompt.java
+++ src/main/java/pairmatching/Message/Excepton/ExceptionPrompt.java
@@ -4,7 +4,8 @@ package pairmatching.Message.Excepton;
 import static pairmatching.Message.Excepton.ExceptionPrefix.ERROR_PREFIX;
 
 public enum ExceptionPrompt {
-    INVALID_INPUT("유효하지 않은 입력입니다. 다시 입력해 주세요.");
+    INVALID_INPUT("유효하지 않은 입력입니다. 다시 입력해 주세요."),
+    NOT_FOUND("매칭 이력이 없습니다."),;
 
     private final String prompt;
 
---
+++ src/main/java/pairmatching/Message/Prompt.java
@@ -0,0 +1,28 @@
+package pairmatching.Message;
+
+public class Prompt {
+
+    public static final String SHOW_INPUT_MENU = """
+            기능을 선택하세요.
+            1. 페어 매칭
+            2. 페어 조회
+            3. 페어 초기화
+            Q. 종료
+            """;
+
+    private static final String SHOW_PAIR_MATCH_OPTION = """
+            #############################################
+            과정: 백엔드 | 프론트엔드
+            미션:
+              - 레벨1: 자동차경주 | 로또 | 숫자야구게임
+              - 레벨2: 장바구니 | 결제 | 지하철노선도
+              - 레벨3:
+              - 레벨4: 성능개선 | 배포
+              - 레벨5:
+            ############################################
+            """;
+
+    private static final String INPUT_COURSE_LEVEL_MISSION = """
+            과정, 레벨, 미션을 선택하세요.
+            ex) 백엔드, 레벨1, 자동차경주""";
+}
